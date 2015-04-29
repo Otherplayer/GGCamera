27e1f51cf7937a5d900ce9329f67496669ea1e3f
zero <osmk@qq.com>
2015-04-29 21:30:22 +0800

    bulid

---
+++ GGCamera/GGCamera.h
@@ -0,0 +1,26 @@
+//
+//  GGCamera.h
+//  __无邪_
+//
+//  Created by __无邪_ on 15/4/28.
+//  Copyright (c) 2015年 __无邪_. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@class GGCamera;
+
+
+@protocol GGCameraDelegate <NSObject>
+
+- (void)camera:(GGCamera *)camera didFinishPickingMediaWithImage:(UIImage *)image;
+
+@end
+@interface GGCamera : NSObject
+@property (nonatomic, unsafe_unretained) id<GGCameraDelegate>delegate;
+@property (nonatomic, strong) void(^resultBlock)(UIImage *image);
++ (instancetype)sharedInstance;
+
+- (void)showCamera;
+- (void)showCameraResult:(void(^)(UIImage *image))resultBlock; /*  */
+- (void)showCameraResultWithUrl:(void (^)(NSString *imageurl))resultBlock;
+@end
